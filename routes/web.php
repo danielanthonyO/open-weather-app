5c70d8d59087fa0e0cfdd40665b494d8bef3a042
daniel.anthony@edu.bc.fi <daniel.anthony@edu.bc.fi>
2025-05-30 08:20:42 +0300

    Updating content of the routes folder

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,11 @@
 use App\Http\Controllers\WeatherController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
-// Route::get('/weather', function () {
-//     return view('weather');
-// })->name('weather');
-
-Route::get('/weather', [WeatherController::class, 'index'])->name('weather');
+Route::get('/', [WeatherController::class, 'index'])->name('weather');
 
 
 
